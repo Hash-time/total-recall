003198f69601b0d5d2335356f46d1b6ddc7884ba
hash-time <90512878+Hash-time@users.noreply.github.com>
2021-09-25 18:20:10 +0300

    Add files via upload

---
+++ задача 1(исправленая).py
@@ -0,0 +1,75 @@
+class Football:
+
+    def team1(self):
+        print('Роналду')
+    def team2(self):
+        print('Неймар')
+    def team3(self):
+        print('Мбаппе')
+    def team4(self):
+        print('Месси')
+    def team5(self):
+        print('Златан')
+    def team6(self):
+        print('Роналдиньо')
+    def team7(self):
+        print('Погба')
+    def team8(self):
+        print('Бэйл')
+    def team9(self):
+        print('Агуэро')
+    def team10(self):
+        print('Брюйне')
+    def team11(self):
+        print('Нойер')
+    def num1(self):
+        print('1')
+    def num2(self):
+        print('2')
+    def num3(self):
+        print('3')
+    def num4(self):
+        print('4')
+    def num5(self):
+        print('5')
+    def num6(self):
+        print('6')
+    def num7(self):
+        print('7')
+    def num8(self):
+        print('8')
+    def num9(self):
+        print('9')
+    def num10(self):
+        print('10')
+    def num11(self):
+        print('11')
+    
+
+
+
+def main():
+    foot=Football()
+    foot.team1()
+    foot.team2()
+    foot.team3()
+    foot.team4()
+    foot.team5()
+    foot.team6()
+    foot.team7()
+    foot.team8()
+    foot.team9()
+    foot.team10()
+    foot.team11()
+    foot.num1()
+    foot.num2()
+    foot.num3()
+    foot.num4()
+    foot.num5()
+    foot.num6()
+    foot.num7()
+    foot.num8()
+    foot.num9()
+    foot.num10()
+    foot.num11()
+main()
